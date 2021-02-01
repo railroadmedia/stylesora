abd370980dbd0316dd64808e522db994375d1587
Miguel Fondeur <miguelfondeur@gmail.com>
2021-02-01 15:01:13 -0500

    add push shell script

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if git diff-index --quiet HEAD --; then
+    # Build the lib and commit the files
+    npm run build:prod;
+    git add -a
+    git commit -am "New build";
+
+    # Create a new patch version
+    npm version patch;
+    PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+    echo 'Creating Tag and Commit for Stylesora Version: ' $PACKAGE_VERSION
+    
+    git push --tags;
+    git push;
+
+    # Publish the Package
+    npm publish;
+else
+    echo 'Local git directory is not clean! Commit your files and try again.'
+fi
+
