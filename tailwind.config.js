c17fba22d8fe53f09727eedbbc5a1009090dfc15
Yalung Tang <yalungtang@gmail.com>
2022-01-19 14:37:32 -0300

    added tailwind v3 compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,31 @@
-const stylesoraTheme = require('./lib/theme');
-const colors = require('tailwindcss/colors');
+const stylesoraTheme = require("./lib/theme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    // mode: 'jit',
-    prefix: 'tw-',
-    important: true,
-    purge: {
-        enabled: false
+  prefix: "tw-",
+  important: true,
+  content: ["./sandbox/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: stylesoraTheme.fontFamily,
+      fontSize: stylesoraTheme.fontSize,
+      colors: {
+        drumeo: stylesoraTheme.colors.drumeo,
+        pianote: stylesoraTheme.colors.pianote,
+        guitareo: stylesoraTheme.colors.guitareo,
+        singeo: stylesoraTheme.colors.singeo,
+        recordeo: stylesoraTheme.colors.recordeo,
+        "true-gray": colors.neutral,
+      },
+      spacing: stylesoraTheme.spacing,
+      zIndex: stylesoraTheme.zIndex,
+      inset: stylesoraTheme.inset,
     },
-    variants: {
-        extend: {
-            backgroundColor: ['active', 'visited'],
-            textColor: ['visited', 'active'],
-        }
-    },
-    theme: {
-        extend: {
-            fontFamily: stylesoraTheme.fontFamily,
-            fontSize: stylesoraTheme.fontSize, 
-            colors: {
-                drumeo: stylesoraTheme.colors.drumeo,
-                pianote: stylesoraTheme.colors.pianote,
-                guitareo: stylesoraTheme.colors.guitareo,
-                singeo: stylesoraTheme.colors.singeo,
-                recordeo: stylesoraTheme.colors.recordeo,
-                "true-gray": colors.trueGray
-            },
-            spacing: stylesoraTheme.spacing,
-            zIndex: stylesoraTheme.zIndex,
-            inset: stylesoraTheme.inset         
-        }
-    },
-    plugins: [
-        require('./lib/base')(),
-        require('./lib/attributes')(),
-        require('./lib/utilities')({
-            variants: ['responsive', 'hover'],
-        }),
-        require('./lib/components')({
-            variants: ['responsive', 'hover'],
-        }),
-    ],
+  },
+  plugins: [
+    require("./lib/base")(),
+    require("./lib/attributes")(),
+    require("./lib/utilities")(),
+    require("./lib/components")(),
+  ],
 };
-
