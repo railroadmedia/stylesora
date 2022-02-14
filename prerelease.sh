258aa4525add1f8a0d41ca8a72c7b0956f21f74e
calebfavor <calebfavor@gmail.com>
2022-02-14 21:24:06 +0000

    update prerelease

--- prerelease.sh
+++ prerelease.sh
@@ -7,12 +7,12 @@ if git diff-index --quiet HEAD --; then
     git commit -am "New pre-release build";
 
     # Create a new patch version
-    npm version prerelease --preid=staging
+    npm version prerelease --preid=3.0
 
     git push;
 
     # Publish the Package
-    npm publish --tag staging;
+    npm publish --tag 3.0;
 else
     echo 'Local git directory is not clean! Commit your files and try again.'
 fi
